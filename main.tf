8a3fb10beb716b577a3bf8550ea41ec72ed9168f
mounika <mounika.gg77@gmail.com>
2023-09-06 19:23:47 +0530

    new changes

--- main.tf
+++ main.tf
@@ -78,5 +78,11 @@ module "application_load_balancer" {
   certificate_arn       = module.ssl_certificate.certificate_arn
 }
 
-
+# create s3 bucket
+module "s3_bucket" {
+  source = "git@github.com:mounikainfo/terraform-modules.git//s3"
+  project_name = local.project_name
+  env_file_bucket_name = var.env_file_bucket_name
+  env_file_name = var.env_file_name
+}
 
