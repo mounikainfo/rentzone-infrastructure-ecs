ff5c953995498d4b5f1df1928ea9dddb028d9d3a
mounika <mounika.gg77@gmail.com>
2023-09-07 16:47:27 +0530

    new changes

--- main.tf
+++ main.tf
@@ -119,14 +119,14 @@ module "ecs_asg" {
   ecs_service  = module.ecs.ecs_service
 }
 
-# create records in route53
+/* # create records in route53
 module "route_53" {
   source                             = "git@github.com:mounikainfo/terraform-modules.git//route-53"
   domain_name                        = module.ssl_certificate.domain_name
   record_name                        = var.record_name
   application_load_balancer_dns_name = module.application_load_balancer.application_load_balancer_dns_name
   application_load_balancer_zone_id  = module.application_load_balancer.application_load_balancer_zone_id
-}
+} */
 
 /* # print the website url
 output "website_url" {
