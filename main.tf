d37717cd42feca2f09774e95d6e1e8d2d385db44
mounika <mounika.gg77@gmail.com>
2023-09-14 13:49:10 +0530

    changes

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ module "sgs" {
   vpc_id = module.vpc.vpc_id
 }
 
-# create security groups for eks
+/* # create security groups for eks
 module "myeks" {
   source                    = "git@github.com:mounikainfo/terraform-modules.git//eks"
   private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
@@ -155,4 +155,4 @@ module "nodes" {
   private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
   private_app_subnet_az2_id = module.vpc.private_app_subnet_az2_id
   cluster_name              = var.cluster_name
-}
+} */
